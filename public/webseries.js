59e1152b1c07572e49d9f49cb280ff51608a49f7
RahulPonnuru <98414565+RahulPonnuru@users.noreply.github.com>
2022-06-25 11:23:38 +0530

    Update webseries.js

--- public/webseries.js
+++ public/webseries.js
@@ -1,6 +1,6 @@
-const API_KEY="api_key=61022532bb5ed4d2a4a36463a039e57f";
+const API_KEY="";
 const BASE_URL="https://api.themoviedb.org/3";
-const API_URL=BASE_URL+"/list/8203256?"+API_KEY;
+const API_URL=BASE_URL+"/list/820325?"+API_KEY;
 const main=document.getElementById('main');
 const IMG_URL="https://image.tmdb.org/t/p/w500";
 
